09f4b7e81de7c0583dcce33ad3d5e35614b4964e
Xogo8565 <Xogo8565@gmail.com>
2022-05-26 14:50:05 +0900

    1

--- src/main/java/com/test/controller/HomeController.java
+++ src/main/java/com/test/controller/HomeController.java
@@ -7,22 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HomeController
- */
-@WebServlet("/index")
+@WebServlet("/home")
 public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-    
     public HomeController() {
-  
+        
+    }
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("/index.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
